62487688873fa6f36a645df65f80d71e4e9a5c54
Yash Joshi <yashjoshi2050@gmail.com>
2025-09-28 09:41:06 +0530

    Split header into component and use Link for speed spa

--- ecommerce-project/src/Pages/HomePage.jsx
+++ ecommerce-project/src/Pages/HomePage.jsx
@@ -1,40 +1,12 @@
 import './HomePage.css'
-import './header.css'
+import { Header } from '../components/header';
 export function HomePage(){
     return (
         <>
+
         <title>HomePage</title>
-            <div class="header">
-            <div class="left-section">
-                <a href="/" class="header-link">
-                <img class="logo"
-                    src="images/logo-white.png" />
-                <img class="mobile-logo"
-                    src="images/mobile-logo-white.png" />
-                </a>
-            </div>
-
-            <div class="middle-section">
-                <input class="search-bar" type="text" placeholder="Search" />
-
-                <button class="search-button">
-                <img class="search-icon" src="images/icons/search-icon.png" />
-                </button>
-            </div>
-
-            <div class="right-section">
-                <a class="orders-link header-link" href="/orders">
-
-                <span class="orders-text">Orders</span>
-                </a>
-
-                <a class="cart-link header-link" href="/checkout">
-                <img class="cart-icon" src="images/icons/cart-icon.png" />
-                <div class="cart-quantity">3</div>
-                <div class="cart-text">Cart</div>
-                </a>
-            </div>
-            </div>
+                
+            <Header></Header>
 
             <div class="home-page">
             <div class="products-grid">
